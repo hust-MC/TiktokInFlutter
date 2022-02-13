9a09344ca59228d7dbc7d8aadc81cc0815d5bef3
hust-MC <251887584@qq.com>
2022-02-13 23:47:45 +0800

    引入flutter

--- app/src/main/java/com/emercy/fluttertik/MainActivity.kt
+++ app/src/main/java/com/emercy/fluttertik/MainActivity.kt
@@ -3,11 +3,19 @@ package com.emercy.fluttertik
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.FragmentActivity
+import io.flutter.embedding.android.FlutterFragment
 
-class MainActivity : Activity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.e("mcmc", "${MainActivity::class.java}")
         setContentView(R.layout.activity_main)
+
+        val fragment = FlutterFragment.createDefault()
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_friend, fragment)
+            .commit()
     }
 }
