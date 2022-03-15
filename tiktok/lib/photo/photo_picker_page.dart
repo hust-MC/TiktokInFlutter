2b52384aed927e1a895b4da41d05010e8afff952
hust-MC <251887584@qq.com>
2022-03-15 12:16:19 +0800

    Add pick image in PhotoPickerPage

--- tiktok/lib/photo/photo_picker_page.dart
+++ tiktok/lib/photo/photo_picker_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tiktok/channel_util.dart';
+
+import '../main.dart';
 
 class PhotoPickerPage extends StatelessWidget {
   final String fileUrl;
@@ -8,8 +12,44 @@ class PhotoPickerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: GestureDetector(
-            child: Image.asset(fileUrl), onTap: () => print('MCLOG, ontap'),), color: Colors.black, height: double.infinity, width: double.infinity);
+        child: Stack(children: [
+          Align(
+            child: GestureDetector(
+                child: Image.asset(fileUrl),
+                onTap: () {
+                  router.popRoute();
+                  ChannelUtil.hideBottomBar(false);
+                }),
+            alignment: Alignment.center,
+          ),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 85),
+                child: GestureDetector(
+                    child: Container(
+                      alignment: Alignment.center,
+                      color: Color(0xff262626),
+                      width: 190,
+                      height: 36,
+                      child: Text(
+                        '更换背景',
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          color: Colors.white70,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                    onTap: () async {
+                      var image = await ImagePicker().pickImage(source: ImageSource.gallery);
+                      print("PICK image: $image");
+                    }),
+              ))
+        ]),
+        color: Colors.black,
+        height: double.infinity,
+        width: double.infinity);
   }
 
   @override
