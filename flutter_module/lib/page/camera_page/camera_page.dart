8b210bffa4ba8fe4ce124f59870821f4a52dabe7
hust-MC <251887584@qq.com>
2022-08-21 02:15:15 +0800

    【14】Camera page ui finish

--- flutter_module/lib/page/camera_page/camera_page.dart
+++ flutter_module/lib/page/camera_page/camera_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter_module/widget/t_image.dart';
+
+import '../../gen/assets.gen.dart';
 
 class CameraPage extends StatefulWidget {
   @override
@@ -7,7 +11,7 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
-  late CameraController controller;
+  CameraController? controller;
   late List<CameraDescription> cameras;
 
   @override
@@ -16,8 +20,8 @@ class _CameraPageState extends State<CameraPage> {
 
     availableCameras().then((value) {
       cameras = value;
-      controller = CameraController(cameras[0], ResolutionPreset.max);
-      controller.initialize().then((_) {
+      controller = CameraController(cameras[0], ResolutionPreset.medium);
+      controller?.initialize().then((_) {
         if (!mounted) {
           return;
         }
@@ -28,22 +32,77 @@ class _CameraPageState extends State<CameraPage> {
 
   @override
   void dispose() {
-    controller.dispose();
+    controller?.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // if (!controller.value.isInitialized) {
-    //   return Container();
-    // }
-    return WillPopScope(
-        onWillPop: () async {
-          print('MCLOG');
-          return true;
-        },
-        child: MaterialApp(
-          home: Text('MCMLOG'),
-        ));
+    return MaterialApp(
+        home: Stack(children: [
+      controller == null || !controller!.value.isInitialized
+          ? Container(color: Colors.black)
+          : Container(
+              width: double.infinity, height: double.infinity, child: CameraPreview(controller!)),
+      Padding(
+          padding: EdgeInsets.only(top: 38, left: 19),
+          child: TImage(Assets.image.close.path, height: 18, width: 18)),
+      Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+              padding: EdgeInsets.only(top: 35, right: 14),
+              child: Column(children: [
+                _buildIcon(Assets.image.rotate.path, '翻转'),
+                SizedBox(height: 16),
+                _buildIcon(Assets.image.clock.path, '倒计时'),
+                SizedBox(height: 16),
+                _buildIcon(Assets.image.flashOn.path, '闪光灯'),
+              ]))),
+      Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: EdgeInsets.only(left: 62, bottom: 110),
+            child: Column(
+              children: [
+                TImage(Assets.image.gallery.path, height: 40, width: 40),
+                SizedBox(height: 10),
+                Text('相册',
+                    style: TextStyle(
+                        color: Colors.white, fontSize: 12, decoration: TextDecoration.none))
+              ],
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+            ),
+          )),
+      Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+              padding: EdgeInsets.only(bottom: 115),
+              child: Container(
+                  width: 75,
+                  height: 75,
+                  child: Padding(
+                    child: TImage(Assets.image.flashOn.path, height: 33, width: 33),
+                    padding: EdgeInsets.symmetric(vertical: 17, horizontal: 22),
+                  ),
+                  decoration: new BoxDecoration(
+                    color: Color(0xffff2c54),
+                    //设置四周圆角 角度
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                    //设置四周边框
+                    border: new Border.all(width: 4, color: Colors.white),
+                  )))),
+    ]));
+  }
+
+  Widget _buildIcon(String iconPath, String title) {
+    return Column(
+      children: [
+        TImage(iconPath, width: 25, height: 25),
+        SizedBox(height: 6),
+        Text(title,
+            style: TextStyle(color: Colors.white, fontSize: 10, decoration: TextDecoration.none))
+      ],
+    );
   }
 }
