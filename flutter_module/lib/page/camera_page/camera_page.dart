86f96b22f396c34089d3188eb64159236cac2988
hust-MC <251887584@qq.com>
2022-08-21 17:17:48 +0800

    【14】【Finish】flash/turn over/delay

--- flutter_module/lib/page/camera_page/camera_page.dart
+++ flutter_module/lib/page/camera_page/camera_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter_module/channel_util.dart';
 import 'package:flutter_module/widget/t_image.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../gen/assets.gen.dart';
 
@@ -14,6 +15,8 @@ class CameraPage extends StatefulWidget {
 class _CameraPageState extends State<CameraPage> {
   CameraController? controller;
   late List<CameraDescription> cameras;
+  var cameraIndex = 0;
+  bool flash = false;
 
   @override
   void initState() {
@@ -21,13 +24,17 @@ class _CameraPageState extends State<CameraPage> {
 
     availableCameras().then((value) {
       cameras = value;
-      controller = CameraController(cameras[0], ResolutionPreset.medium);
-      controller?.initialize().then((_) {
-        if (!mounted) {
-          return;
-        }
-        setState(() {});
-      });
+      _initCameraController();
+    });
+  }
+
+  void _initCameraController() {
+    controller = CameraController(cameras[cameraIndex], ResolutionPreset.medium);
+    controller?.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
     });
   }
 
@@ -58,46 +65,76 @@ class _CameraPageState extends State<CameraPage> {
           child: Padding(
               padding: EdgeInsets.only(top: 35, right: 14),
               child: Column(children: [
-                _buildIcon(Assets.image.rotate.path, '翻转'),
+                GestureDetector(
+                    child: _buildIcon(Assets.image.rotate.path, '翻转'),
+                    onTap: () {
+                      cameraIndex = cameraIndex == 0 ? 1 : 0;
+                      _initCameraController();
+                    }),
                 SizedBox(height: 16),
-                _buildIcon(Assets.image.clock.path, '倒计时'),
+                GestureDetector(
+                    child: _buildIcon(Assets.image.clock.path, '倒计时'),
+                    onTap: () {
+                      Future.delayed(Duration(seconds: 3), () {
+                        _takeAndUpload();
+                      });
+                    }),
                 SizedBox(height: 16),
-                _buildIcon(Assets.image.flashOn.path, '闪光灯'),
+                GestureDetector(
+                  child: _buildIcon(
+                      flash ? Assets.image.flashOn.path : Assets.image.flashOff.path, '闪光灯'),
+                  onTap: () {
+                    controller?.setFlashMode(flash ? FlashMode.off : FlashMode.always);
+                    setState(() {
+                      flash = !flash;
+                    });
+                  },
+                )
               ]))),
       Align(
           alignment: Alignment.bottomLeft,
           child: Padding(
-            padding: EdgeInsets.only(left: 62, bottom: 110),
-            child: Column(
-              children: [
-                TImage(Assets.image.gallery.path, height: 40, width: 40),
-                SizedBox(height: 10),
-                Text('相册',
-                    style: TextStyle(
-                        color: Colors.white, fontSize: 12, decoration: TextDecoration.none))
-              ],
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.end,
-            ),
-          )),
+              padding: EdgeInsets.only(left: 62, bottom: 110),
+              child: GestureDetector(
+                child: Column(
+                  children: [
+                    TImage(Assets.image.gallery.path, height: 40, width: 40),
+                    SizedBox(height: 10),
+                    Text('相册',
+                        style: TextStyle(
+                            color: Colors.white, fontSize: 12, decoration: TextDecoration.none))
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                ),
+                onTap: () async {
+                  var pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+                  var path = pickedFile?.path;
+                  if (path != null) {
+                    print("upload picture: $path");
+                  }
+                },
+              ))),
       Align(
           alignment: Alignment.bottomCenter,
           child: Padding(
               padding: EdgeInsets.only(bottom: 115),
-              child: Container(
-                  width: 75,
-                  height: 75,
-                  child: Padding(
-                    child: TImage(Assets.image.flashOn.path, height: 33, width: 33),
-                    padding: EdgeInsets.symmetric(vertical: 17, horizontal: 22),
-                  ),
-                  decoration: new BoxDecoration(
-                    color: Color(0xffff2c54),
-                    //设置四周圆角 角度
-                    borderRadius: BorderRadius.all(Radius.circular(50)),
-                    //设置四周边框
-                    border: new Border.all(width: 4, color: Colors.white),
-                  )))),
+              child: GestureDetector(
+                  child: Container(
+                      width: 75,
+                      height: 75,
+                      child: Padding(
+                        child: TImage(Assets.image.flashOn.path, height: 33, width: 33),
+                        padding: EdgeInsets.symmetric(vertical: 17, horizontal: 22),
+                      ),
+                      decoration: new BoxDecoration(
+                        color: Color(0xffff2c54),
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        border: new Border.all(width: 4, color: Colors.white),
+                      )),
+                  onTap: () {
+                    _takeAndUpload();
+                  }))),
     ]));
   }
 
@@ -110,5 +147,10 @@ class _CameraPageState extends State<CameraPage> {
             style: TextStyle(color: Colors.white, fontSize: 10, decoration: TextDecoration.none))
       ],
     );
+  }
+
+  void _takeAndUpload() {
+    controller?.takePicture();
+    print("MOOC, upload");
   }
 }
